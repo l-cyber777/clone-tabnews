487109e088b235666ad1e84da417e4ea2827d7b1
Caique Lourenço <lourencoinfoprodutor01@gmail.com>
2026-03-23 00:35:21 +0000

    modifica a frase

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Isabella Perejon, eu amo você. Se você me ama da uma risadinha!</h1>
+    return <h1>Isabella Perejon, eu amo você. Se você me ama da uma risadinha e manda uma foto sorrindo!😀</h1>
 }
 
 export default Home;

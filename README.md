b26ef821308ae06073d1e9f573895c662196d89e
Caique Lourenço <lourencoinfoprodutor01@gmail.com>
2026-03-26 22:12:42 +0000

    adiciona scripts  e

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 Um projeto para treinar programação, aprendendo de ponta à ponta como criar um projeto.
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,10 @@
-function Home() {       
-    return <h1>Isabella Perejon, eu amo você. Se você me ama da uma risadinha e manda uma foto sorrindo!😀</h1>
+function Home() {
+  return (
+    <h1>
+      Isabella Perejon, eu amo você. Se você me ama da uma risadinha e manda uma
+      foto sorrindo!😀
+    </h1>
+  );
 }
 
 export default Home;
